1bf5b93118dabeefb689ea180e44050b3e537964
raidenot <23019328@myrp.edu.sg>
2024-12-03 11:35:43 +0800

    Lesson 3 Exercise 1

--- App.js
+++ App.js
@@ -1,20 +1,103 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState}  from 'react';
+import {View, Text, TextInput, Button, Alert, TouchableOpacity, ToastAndroid} from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
 
-export default function App() {
+// Custom Components
+const InputBox = ({label, onChangeText})=> {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View>
+        <Text>{label}</Text>
+        <TextInput style={{borderWidth: 1}} onChangeText={onChangeText} />
+      </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const MyApp = ()=> {
+  // let username = "";
+  // let pw = "";
+  const [username, setUsername] = useState('');
+  const [pw, setPw] = useState('');
+  const [type, setType] = useState('');
+
+  return (
+      <View style={{padding: 20, paddingTop: 50}} >
+
+        {/*EXERCISE 1B*/}
+        <Text>User Type:</Text>
+        <RNPickerSelect
+            onValueChange={(value) => setType(value)}
+            items={[
+              { label: 'Admin', value: 'Admin' },
+              { label: 'Guest', value: 'Guest' }
+            ]}
+        />
+
+        {/*State*/}
+        {/*<Text>Username:</Text>*/}
+        {/*<TextInput style={{borderWidth: 1}}*/}
+        {/*           onChangeText={(text) => setUsername(text)}*/}
+        {/*/>*/}
+
+        {/*<Text>Password:</Text>*/}
+        {/*<TextInput style={{borderWidth: 1}}*/}
+        {/*           onChangeText={(text) => setPw(text)}*/}
+        {/*/>*/}
+        {/*<Text>{username}</Text>*/}
+        {/*<TouchableOpacity onPress={()=>ToastAndroid.show(pw, ToastAndroid.SHORT)}>*/}
+        {/*    <Text>LOG IN</Text>*/}
+        {/*</TouchableOpacity>*/}
+
+        {/*<Text>{pw}</Text>*/}
+
+        {/*/!*Getting Text input values*!/*/}
+        {/*<Text>Username:</Text>*/}
+        {/*<TextInput style={{borderWidth: 1}}*/}
+        {/*           onChangeText={(text) => username = text}/>*/}
+
+        {/*<Text>Password:</Text>*/}
+        {/*<TextInput style={{borderWidth: 1}}*/}
+        {/*             onChangeText={(text) => pw = text}/>*/}
+
+        {/*<TouchableOpacity onPress={()=>ToastAndroid.show("HELLO " + username + " " + pw, ToastAndroid.SHORT)}>*/}
+        {/*      <Text>LOG IN</Text>*/}
+        {/*</TouchableOpacity>*/}
+
+        {/*EXERCISE 1A*/}
+        {/*<Text>Password:</Text>*/}
+        {/*<TextInput style={{borderWidth: 1}} />*/}
+
+        {/*/!*EXERCISE 1C & 1D*!/*/}
+        {/*<Button*/}
+        {/*    title="LOG IN"*/}
+        {/*    onPress={()=>Alert.alert("Welcome!")}*/}
+        {/*    //onPress={() => ToastAndroid.show("Welcome!", ToastAndroid.SHORT)}*/}
+        {/*/>*/}
+
+        {/*/!*EXERCISE 1E*!/*/}
+        {/*<TouchableOpacity*/}
+        {/*    onPress={()=>Alert.alert("2nd Welcome!")}>*/}
+        {/*    <Text>LOG IN</Text>*/}
+        {/*</TouchableOpacity>*/}
+
+        {/*/!*EXERCISE 1F*!/*/}
+        {/*<TouchableOpacity*/}
+        {/*    onPress={() => ToastAndroid.show("Welcome!", ToastAndroid.SHORT)}>*/}
+        {/*    <Text>LOG IN 2</Text>*/}
+        {/*</TouchableOpacity>*/}
+
+        {/*EXERCISE 1G*/}
+        <InputBox label="User Name:" onChangeText={(text) => setUsername(text)}/>
+
+        <InputBox label="Password:" onChangeText={(text) => setPw(text)}/>
+
+        <TouchableOpacity onPress={()=>
+            ToastAndroid.show('Welcome ' + type + ' ' + username, ToastAndroid.SHORT)}>
+          <Text>LOG IN</Text>
+        </TouchableOpacity>
+        <Text>{pw}</Text>
+
+      </View>
+  );
+};
+
+export default MyApp;
